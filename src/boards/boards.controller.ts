0c3d8315570c9eb96cbb91c9b52193fff2626974
Lrbll <dlxkdxs@naver.com>
2024-03-16 22:23:26 +0900

    typeorm 버전 0.2 -> 0.3 변경 및 관련 코드 수정

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,42 +1,54 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
+import { BoardStatus } from './board-status.enum';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { create } from 'domain';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
+import { Board } from './board.entity';
 
 @Controller('boards')
 export class BoardsController {
     constructor(private boardsService: BoardsService) {}
     
-    @Get('/') /* boards'/' */
-    getAllBoard(): Board[] {
-        return this.boardsService.getAllBoards();
-    }
+    // @Get('/') /* boards'/' */
+    // getAllBoard(): Board[] {
+    //     return this.boardsService.getAllBoards();
+    // }
     
+    // @Post()
+    // @UsePipes(ValidationPipe)
+    // createBoard(
+    //     @Body() createBoardDto: CreateBoardDto
+    // ): Board {
+    //     return this.boardsService.createBoard(createBoardDto);
+    // }
+
     @Post()
     @UsePipes(ValidationPipe)
-    createBoard(
-        @Body() createBoardDto: CreateBoardDto
-    ): Board {
+    createBoard(@Body() createBoardDto: CreateBoardDto): Promise<Board> {
         return this.boardsService.createBoard(createBoardDto);
     }
 
     @Get('/:id')
-    getBoardById(@Param('id') id: String): Board {
-        return this.boardsService.getBoardById(id)
+    getBoardById(@Param('id') id: number) : Promise<Board> {
+        return this.boardsService.getBoardById(id);
     }
 
-    @Delete('/:id')
-    deleteBoard(@Param('id') id: String): void {
-        this.boardsService.deleteBoard(id);
-    }
+    // @Get('/:id')
+    // getBoardById(@Param('id') id: String): Board {
+    //     return this.boardsService.getBoardById(id)
+    // }
 
-    @Patch('/:id/status')
-    updateBoardStatus(
-        @Param('id') id: String,
-        @Body('status', BoardStatusValidationPipe) status: BoardStatus
-    ) {
-        return this.boardsService.updateBoardStatus(id, status);
-    }
+    // @Delete('/:id')
+    // deleteBoard(@Param('id') id: String): void {
+    //     this.boardsService.deleteBoard(id);
+    // }
+
+    // @Patch('/:id/status')
+    // updateBoardStatus(
+    //     @Param('id') id: String,
+    //     @Body('status', BoardStatusValidationPipe) status: BoardStatus
+    // ) {
+    //     return this.boardsService.updateBoardStatus(id, status);
+    // }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,48 +1,76 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
+import { BoardStatus } from './board-status.enum';
 import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Board } from './board.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class BoardsService {
-    private boards: Board[] = [];
-    // Board[] : Board의 타입도 여러개가 들어갈 수 있도록 배열로
-    getAllBoards():Board[] {
-        return this.boards;
-    }
+    constructor(
+        @InjectRepository(Board)
+        private boardRepository: Repository<Board>,
+    ) {}
+    // getAllBoards():Board[] {
+    //     return this.boards;
+    // }
 
-    createBoard(createBoardDto: CreateBoardDto) {
-        const { title, description } = createBoardDto;
-        // const title = createBoardDto.title;
+    // createBoard(createBoardDto: CreateBoardDto) {
+    //     const { title, description } = createBoardDto;
+    //     // const title = createBoardDto.title;
 
-        const board: Board = {
-            id: uuid(),
-            title: title, // 이름이 같을 경우는 title, 만 작성해도 됨
-            description: description,
+    //     const board: Board = {
+    //         id: uuid(),
+    //         title: title, // 이름이 같을 경우는 title, 만 작성해도 됨
+    //         description: description,
+    //         status: BoardStatus.PUBLIC
+    //     }
+
+    //     this.boards.push(board);
+    //     return board;
+    // }
+
+    async createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
+        const {title, description} = createBoardDto;
+
+        const board = this.boardRepository.create({
+            title,
+            description,
             status: BoardStatus.PUBLIC
-        }
+        })
 
-        this.boards.push(board);
+        await this.boardRepository.save(board);
         return board;
     }
 
-    getBoardById(id: String): Board {
-        const found = this.boards.find((board) => board.id === id);
-        
+    async getBoardById(id: number): Promise<Board> {
+        const found = await this.boardRepository.findOneBy({ id });
+
         if(!found) {
-            throw new NotFoundException(`Can't find Board with id ${id}`);
+            throw new NotFoundException(`Can't find Board with id ${id}`)
         }
+
         return found;
     }
 
-    deleteBoard(id: String): void {
-        const found = this.getBoardById(id);
-        this.boards = this.boards.filter((board) => board.id !== found.id);
-    }
+    // getBoardById(id: String): Board {
+    //     const found = this.boards.find((board) => board.id === id);
+        
+    //     if(!found) {
+    //         throw new NotFoundException(`Can't find Board with id ${id}`);
+    //     }
+    //     return found;
+    // }
 
-    updateBoardStatus(id: String, status: BoardStatus): Board {
-        const board = this.getBoardById(id);
-        board.status = status;
-        return board;
-    }
+    // deleteBoard(id: String): void {
+    //     const found = this.getBoardById(id);
+    //     this.boards = this.boards.filter((board) => board.id !== found.id);
+    // }
+
+    // updateBoardStatus(id: String, status: BoardStatus): Board {
+    //     const board = this.getBoardById(id);
+    //     board.status = status;
+    //     return board;
+    // }
 }
